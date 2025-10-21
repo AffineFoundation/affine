6d351de23849b8d0df19c8e93fe553dac8ee58d7
angosr <unangosr@gmail.com>
2025-10-21 23:47:36 +0800

    AFFINE_MAX_CONCURRENCY defaults to 20 (#114)

--- affine/cli.py
+++ affine/cli.py
@@ -71,7 +71,7 @@ def runner():
         SINK_BATCH_SIZE = 300
         SINK_MAX_WAIT = 300
         STATUS_LOG_INTERVAL = 30
-        MAX_CONCURRENCY = int(os.getenv("AFFINE_MAX_CONCURRENCY", "10"))
+        MAX_CONCURRENCY = int(os.getenv("AFFINE_MAX_CONCURRENCY", "20"))
 
         # Shared state
         miners_map: Dict[int, any] = {}
