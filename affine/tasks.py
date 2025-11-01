d5f637bdefacdaa75482cd8f6a2a34a4135cff4d
angosr <unangosr@gmail.com>
2025-11-01 17:02:02 +0000

    mem limit 10g

--- affine/tasks.py
+++ affine/tasks.py
@@ -174,6 +174,7 @@ class BaseSDKEnv(ABC):
                 hosts=hosts,
                 replicas=replicas,
                 container_name=container_name,
+                mem_limit="10g",
                 pull=True,
                 force_recreate=True,
             )
